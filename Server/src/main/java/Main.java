03645cfdd277a51c2ee5b2784caf43e225834417
Artur Batskalevich <artur.batskalevich@gmail.com>
2022-01-13 14:10:17 +0300

    add client.iml

---
+++ Server/src/main/java/Main.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("server started");
+        int port = 8990;
+
+        String adult = "Welcome to the adult zone, %s Have a good rest, or a good working day!";
+        String child = "Welcome to the kids area, %s Let's play!";
+
+        while (true) {
+            try {
+                ServerSocket serverSocket = new ServerSocket(port);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                out.println("Write your name");
+                String nameClient = in.readLine();
+                out.println("Are you child? (yes/no)");
+                String childYesOrNo = in.readLine();
+
+                switch (childYesOrNo){
+                    case "yes":
+                        out.println(String.format(child,nameClient));
+                        break;
+                    case "no" :
+                        out.println(String.format(adult,nameClient));
+                        break;
+                    default:
+                        out.println("Некорректные данные");
+                }
+
+                System.out.printf("New connection accepted. Port: %d%n",clientSocket.getPort());
+                final String name = in.readLine();
+                out.println(String.format("Hi %s, your port is %d",name,clientSocket.getPort()));
+                serverSocket.close();
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
---
+++ client/src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+        int port = 8990;
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            Socket clientSocket = new Socket(host,port);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String name = in.readLine();
+            System.out.println(name);
+            String result = scanner.nextLine();
+            out.println(result);
+
+            String childYes = in.readLine();
+            System.out.println(childYes);
+            String result1 = scanner.nextLine();
+            out.println(result1);
+
+            String serverResult = in.readLine();
+            System.out.println(serverResult);
+
+//            out.println("Netology");
+//            String resp = in.readLine();
+//            System.out.println(resp);
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
